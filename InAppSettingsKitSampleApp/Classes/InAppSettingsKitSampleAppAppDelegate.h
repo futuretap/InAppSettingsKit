3d545ef41ff35b2de04029d57931eca51ecc4614
StuFF mc <mc@stuffmc.com>
2010-03-11 12:28:08 +0100

    Added TabBarController to show that it works also with it, Added Icon (glyphish.com) for TabBar & App Icon, Added French Localization, Added a .gitignore file, changed app name to "IASK" since "InAppSettingsKitSample" is a bit long :-)

--- InAppSettingsKitSampleApp/Classes/InAppSettingsKitSampleAppAppDelegate.h
+++ InAppSettingsKitSampleApp/Classes/InAppSettingsKitSampleAppAppDelegate.h
@@ -20,10 +20,12 @@
 @interface InAppSettingsKitSampleAppAppDelegate : NSObject <UIApplicationDelegate> {
     UIWindow *window;
     UINavigationController *navigationController;
+    UITabBarController *tabBarController;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
 
 @end
 
