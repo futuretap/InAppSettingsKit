15e29762908527ae87ae1b5ee0fadd748e791106
Marc-Etienne M.Léveillé <marc-etienne@ulap.local>
2010-11-10 10:18:33 -0500

    Fixed headings to match other source files

--- InAppSettingsKit/Models/IASKSettingsWriterToFile.h
+++ InAppSettingsKit/Models/IASKSettingsWriterToFile.h
@@ -1,9 +1,18 @@
 //
 //  IASKSettingsWriterToFile.h
-//  Porthole
+//  http://www.inappsettingskit.com
 //
-//  Created by Marc-Etienne M.Léveillé on 10-11-04.
-//  Copyright 2010 Edovia. All rights reserved.
+//  Copyright (c) 2010:
+//  Luc Vandal, Edovia Inc., http://www.edovia.com
+//  Ortwin Gentz, FutureTap GmbH, http://www.futuretap.com
+//  Marc-Etienne M.Léveillé, Edovia Inc., http://www.edovia.com
+//  All rights reserved.
+// 
+//  It is appreciated but not required that you give credit to Luc Vandal and Ortwin Gentz, 
+//  as the original authors of this code. You can give credit in a blog post, a tweet or on 
+//  a info page of your app. Also, the original authors appreciate letting them know if you use this code.
+//
+//  This code is licensed under the BSD license that is available at: http://www.opensource.org/licenses/bsd-license.php
 //
 
 #import <Foundation/Foundation.h>
