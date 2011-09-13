c34def93884e6139654602101cd044663c5fc363
Ortwin Gentz <iask@futuretap.com>
2011-09-13 16:17:12 +0200

    Sample app: always allow UI rotation, fix tabBar integration for iPad;

--- InAppSettingsKitSampleApp/CustomSubviewController.m
+++ InAppSettingsKitSampleApp/CustomSubviewController.m
@@ -37,13 +37,9 @@
 }
 */
 
-/*
-// Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
-*/
 
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
