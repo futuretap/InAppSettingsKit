ac64620817a9ce15b4eab739ab252569713c6809
Stephan Diederich <stephan.diederich@googlemail.com>
2012-12-19 17:00:25 +0100

    add static library project for IASK
    * add static library project
      provide proper build settings, formatting, unit tests
    * add workspace to repository (combines app + static lib project)
    * modify sources to work with static lib project
      the sample project uses recursive-header search paths to find
      IASK headers in the different subfolder (Controllers/.. Models/,…).
      The static lib sample app uses the installed headers which are installed
      in InAppSettingsKit/ subfolder
    * prepare unit tests for IASK which will live in the static library project
    * static lib project configuration is done in xcconfig files for easier maintenance

---
+++ InAppSettingsKitTests/IASSettingsStoreUserDefaultsTests.m
@@ -0,0 +1,30 @@
+//
+//  IASSettingsStoreUserDefaultsTests.m
+//  IASSettingsStoreUserDefaultsTests
+//
+//  Created by Stephan Diederich on 19.12.12.
+//  Copyright (c) 2012 InAppSettingsKit. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "IASKSettingsStoreUserDefaults.h"
+
+@interface IASSettingsStoreUserDefaultsTests : SenTestCase
+
+@end
+
+@implementation IASSettingsStoreUserDefaultsTests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
