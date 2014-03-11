4ab708a9a9f548f717fcaeeb64c60d0e24ea31a3
Jens Daemgen <jens.daemgen@cocologics.com>
2014-03-11 18:07:37 +0100

    Fix build under Xcode 5.1 by removing unused vars

--- InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
+++ InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
@@ -31,10 +31,6 @@
 #error "IASK needs ARC"
 #endif
 
-static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3f;
-static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2f;
-static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8f;
-
 static NSString *kIASKCredits = @"Powered by InAppSettingsKit"; // Leave this as-is!!!
 
 #define kIASKSpecifierValuesViewControllerIndex       0
