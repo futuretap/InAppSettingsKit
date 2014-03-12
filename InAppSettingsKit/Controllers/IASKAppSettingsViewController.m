c20b25d640029f4d2f39ed74913f3effb649bacd
Ortwin Gentz <gentz@futuretap.com>
2014-03-12 13:25:58 +0100

    Fixed UISwitch animation
    due to a feedback loop via the NSUserDefaultsDidChangeNotification, the UISwitch was reloaded before the animation finished

--- InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
+++ InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
@@ -836,7 +836,9 @@ static NSDictionary *oldUserDefaults = nil;
 		}
 	}
 	if (indexPathsToUpdate.count) {
-		[self.tableView reloadRowsAtIndexPaths:indexPathsToUpdate withRowAnimation:UITableViewRowAnimationNone];
+		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+			[self.tableView reloadRowsAtIndexPaths:indexPathsToUpdate withRowAnimation:UITableViewRowAnimationNone];
+		});
 	}
 }
 
