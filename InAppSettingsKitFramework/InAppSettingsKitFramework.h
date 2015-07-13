731f68b31cff9c8240594b6d88cf135ff36e83ff
Paul Cantrell <cantrell@pobox.com>
2015-07-13 16:00:19 -0500

    Added Framework target for Carthage support

---
+++ InAppSettingsKitFramework/InAppSettingsKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  InAppSettingsKitFramework.h
+//  InAppSettingsKitFramework
+//
+//  Created by Paul on 2015/7/13.
+//  Copyright © 2015 InAppSettingsKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppSettingsKitFramework.
+FOUNDATION_EXPORT double InAppSettingsKitFrameworkVersionNumber;
+
+//! Project version string for InAppSettingsKitFramework.
+FOUNDATION_EXPORT const unsigned char InAppSettingsKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppSettingsKitFramework/PublicHeader.h>
+
+
