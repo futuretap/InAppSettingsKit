df2e52780a5abd7b3af102adb01ef9752f43f408
Ortwin Gentz <gentz@futuretap.com>
2016-02-17 12:01:29 +0100

    Fixed scrolling to top row in multiValue lists
    Fixed #331

--- InAppSettingsKit/Controllers/IASKSpecifierValuesViewController.m
+++ InAppSettingsKit/Controllers/IASKSpecifierValuesViewController.m
@@ -24,7 +24,7 @@
 @interface IASKSpecifierValuesViewController()
 
 @property (nonatomic, strong, readonly) IASKMultipleValueSelection *selection;
-
+@property (nonatomic) BOOL didFirstLayout;
 @end
 
 @implementation IASKSpecifierValuesViewController
@@ -62,17 +62,27 @@
     
     if (_tableView) {
         [_tableView reloadData];
-
-		// Make sure the currently checked item is visible
-        [_tableView scrollToRowAtIndexPath:_selection.checkedItem
-                          atScrollPosition:UITableViewScrollPositionMiddle animated:NO];
     }
+	self.didFirstLayout = NO;
 	[super viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-	[_tableView flashScrollIndicators];
 	[super viewDidAppear:animated];
+	[_tableView flashScrollIndicators];
+}
+
+- (void)viewDidLayoutSubviews {
+	[super viewDidLayoutSubviews];
+
+	if (!self.didFirstLayout) {
+		// Make sure the currently checked item is visible
+		// this needs to be done as early as possible when pushing the view but after the first layout
+		// otherwise scrolling to the first entry doesn't respect tableView.contentInset
+		[_tableView scrollToRowAtIndexPath:_selection.checkedItem
+						  atScrollPosition:UITableViewScrollPositionMiddle animated:NO];
+		self.didFirstLayout = YES;
+	}
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
