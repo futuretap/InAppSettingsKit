140f7ebf47b2fd6f9080de8cd3df6e69f44b0f23
Wojciech Adam Koszek <wojciech@koszek.com>
2016-07-07 22:39:43 -0700

    Bring the sample app.

---
+++ InAppSettingsKitSampleAppStoryboard/InAppSettingsKitSampleAppStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InAppSettingsKitSampleAppStoryboard
+//
+//  Created by Wojciech Adam Koszek (h) on 07/07/2016.
+//  Copyright © 2016 InAppSettingsKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
