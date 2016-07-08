7b8ba3a4b79bf5061fc168e18fd088c940ab7e0d
Wojciech Adam Koszek <wojciech@koszek.com>
2016-07-07 22:48:53 -0700

    Show how to use neverShowPrivacy... and credits.

--- InAppSettingsKitSampleAppStoryboard/InAppSettingsKitSampleAppStoryboard/SettingsTableTableViewController.m
+++ InAppSettingsKitSampleAppStoryboard/InAppSettingsKitSampleAppStoryboard/SettingsTableTableViewController.m
@@ -17,17 +17,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+    // To hide the privacy cells, if any.
+    self.neverShowPrivacySettings = YES;
+
+    // To hide the credits footer. Remember to give IASK a credit in your
+    //  "Credits" screen.
+    self.showCreditsFooter = NO;
 }
 
 /*
