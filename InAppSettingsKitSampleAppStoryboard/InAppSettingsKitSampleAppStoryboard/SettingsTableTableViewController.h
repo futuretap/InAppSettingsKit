0c0366fe79b028dbafcc0de6ab2b2e18bdbd8dba
Wojciech Adam Koszek <wojciech@koszek.com>
2016-07-07 22:39:43 -0700

    Bring the sample app.

---
+++ InAppSettingsKitSampleAppStoryboard/InAppSettingsKitSampleAppStoryboard/SettingsTableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableTableViewController.h
+//  InAppSettingsKitSampleAppStoryboard
+//
+//  Created by Wojciech Adam Koszek (h) on 07/07/2016.
+//  Copyright © 2016 InAppSettingsKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableTableViewController : UITableViewController
+
+@end
---
+++ InAppSettingsKitSampleAppStoryboard/InAppSettingsKitSampleAppStoryboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InAppSettingsKitSampleAppStoryboard
+//
+//  Created by Wojciech Adam Koszek (h) on 07/07/2016.
+//  Copyright © 2016 InAppSettingsKit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
