3db9f7d4cf7cf58a5eb01809a61cc8bb4f6722c9
valvoline <costantino@sofapps.it>
2019-09-17 18:26:59 +0200

    IASKTextView: Fixed an issue with darkMode on iOS13+

---
+++ InAppSettingsKit/Views/IASKColor.h
@@ -0,0 +1,19 @@
+//
+//  IASKColor.h
+//  InAppSettingsKit
+//
+//  Created by valvoline on 17/09/2019.
+//  Copyright © 2019 InAppSettingsKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IASKColor : UIColor
+
++ (UIColor *)iaskPlaceholderColor;
+
+@end
+
+NS_ASSUME_NONNULL_END
